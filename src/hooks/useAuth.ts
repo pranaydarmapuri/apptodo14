e89710ce61ecc7d8a22108f48a51314259cf24c1
Srinivas-tech-code <139474565+Srinivas-tech-code@users.noreply.github.com>
2024-02-02 15:37:13 +0530

    Update useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -29,41 +29,50 @@ export default function useAuth() {
   const [jwtToken, setJwtToken] = useState<string>('');
 
 
-   const login = async (username: string, password: string) => {
+  const login = async (username: string, password: string) => {
     try {
       const response = await axios.post<{
         user: {
           passwordHash: string;
-          username: string; token: string; 
-};
-        jwtToken: { token: string; }; token: string 
-}>('http://localhost:3000/api/auth/loginRoute',  JSON.stringify({
+          username: string;
+          token: string;
+        };
+        jwtToken: { token: string };
+      }>('http://localhost:3000/api/auth/loginRoute', JSON.stringify({
         username,
         password,
-      }),{headers:{"Content-Type":'application/json'}});
-      console.log("useAuth login method response =>", response)
-      
-      
+      }), { headers: { 'Content-Type': 'application/json' } });
+  
+      console.log('Login API response:', response);
+  
       setIsAuthenticated(true);
-      if(response && response.data && response.data.jwtToken && response.data.user) {
-        console.log("useAuth login inside defined method response =>", response)
+  
+      if (response && response.data && response.data.jwtToken && response.data.user) {
+        console.log('User data:', response.data.user);
+        console.log('JWT Token from API:', response.data.jwtToken.token);
+      
         setUserState({
           username: response.data.user.username,
-          passwordHash: response.data.user.passwordHash
+          passwordHash: response.data.user.passwordHash,
         });
-        setJwtToken(response.data.jwtToken.token)
-        localStorage.setItem('token', response.data.jwtToken.token);
-      }
-
       
-    } catch (error: AxiosError | any) {
+        setJwtToken(response.data.jwtToken.token);
+      
+        
+        if (localStorage.getItem('token') !== response.data.jwtToken.token) {
+          console.error('Token not saved in localStorage.');
+        } else {
+          console.log('Token saved in localStorage.');
+        }
+      }
+       } catch (error: AxiosError | any) {
       setIsAuthenticated(false);
       console.error('Error logging in', error.response?.data || error.message);
-      
     }
   }
-
+  
    const signUp = () => {
+    
 
   }
 
